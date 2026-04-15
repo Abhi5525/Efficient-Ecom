3604ebdb9d070893ae19c371ee0360bc719f293b
Abhishek <hp7320797@gmail.com>
2026-04-15 14:57:20 +0545

    Refactor settings to use environment variables for email configuration and add .env to .gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -135,5 +135,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS ='True'
-EMAIL_HOST_USER ='codr4570@gmail.com'
-EMAIL_HOST_PASSWORD="qtqadslppcgbhzsc"
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD= os.getenv('EMAIL_PASSWORD')
